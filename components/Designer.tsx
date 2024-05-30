8df1c366c1a872bd1369a807bcb19d0b8792fe28
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-05-30 22:46:21 +0200

    Set Drag form items

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -2,7 +2,12 @@
 
 import { cn } from '@/lib/utils';
 import DesignerSidebar from './DesignerSidebar';
-import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
+import {
+  DragEndEvent,
+  useDndMonitor,
+  useDraggable,
+  useDroppable,
+} from '@dnd-kit/core';
 import useDesigner from './hooks/useDesigner';
 import {
   ElementsType,
@@ -10,6 +15,9 @@ import {
   FormElements,
 } from './FormElements';
 import { idGenerator } from '@/lib/idGenerator';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { BiSolidTrash } from 'react-icons/bi';
 
 const Designer = () => {
   const { elements, addElement } = useDesigner();
@@ -45,7 +53,7 @@ const Designer = () => {
               Drop here
             </p>
           )}
-          {droppable.isOver && (
+          {droppable.isOver && elements.length === 0 && (
             <div className='p-4 w-full'>
               <div className='h-[120px] rounded-md bg-primary/20'></div>
             </div>
@@ -69,9 +77,87 @@ const DesignerElementWrapper = ({
 }: {
   element: FormElementInstance;
 }) => {
+  const { removeElement } = useDesigner();
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+  const topHalf = useDroppable({
+    id: `${element.id}-top`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isTopHalfDesignerElement: true,
+    },
+  });
+  const bottomHalf = useDroppable({
+    id: `${element.id}-bottom`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isBottomHalfDesignerElement: true,
+    },
+  });
+  const draggable = useDraggable({
+    id: `${element.id}-drag-handler`,
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isDesignerElement: true,
+    },
+  });
+  if (draggable.isDragging) return null;
   const DesignerElement = FormElements[element.type].designerComponent;
 
-  return <DesignerElement elementInstance={element} />;
+  return (
+    <div
+      ref={draggable.setNodeRef}
+      {...draggable.listeners}
+      {...draggable.attributes}
+      className='relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset'
+      onMouseEnter={() => setMouseIsOver(true)}
+      onMouseLeave={() => setMouseIsOver(false)}
+    >
+      <div
+        ref={topHalf.setNodeRef}
+        className='absolute w-full h-1/2 rounded-t-md'
+      ></div>
+      <div
+        ref={bottomHalf.setNodeRef}
+        className='absolute w-full bottom-0 h-1/2 rounded-b-md'
+      ></div>
+      {mouseIsOver && (
+        <>
+          <div className='absolute right-0 h-full'>
+            <Button
+              variant={'outline'}
+              className='flex justify-center h-full border rounded-md rounded-l-none bg-red-500'
+              onClick={() => removeElement(element.id)}
+            >
+              <BiSolidTrash className='h-6 w-6' />
+            </Button>
+          </div>
+          <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse'>
+            <p className='text-muted-foreground text-sm'>
+              Click for properties or drag to move
+            </p>
+          </div>
+        </>
+      )}
+      {topHalf.isOver && (
+        <div className='absolute top-0 w-full rounded-md h-[7px] bg-primary rounded-b-none' />
+      )}
+
+      <div
+        className={cn(
+          'flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100',
+          mouseIsOver && 'opacity-30'
+        )}
+      >
+        {bottomHalf.isOver && (
+          <div className='absolute bottom-0 w-full rounded-md h-[7px] bg-primary rounded-t-none' />
+        )}
+        <DesignerElement elementInstance={element} />
+      </div>
+    </div>
+  );
 };
 
 export default Designer;
--- components/context/DesignerContext.tsx
+++ components/context/DesignerContext.tsx
@@ -6,6 +6,7 @@ import { FormElementInstance } from '../FormElements';
 type DesignerContextType = {
   elements: FormElementInstance[];
   addElement: (index: number, element: FormElementInstance) => void;
+  removeElement: (id: string) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -21,8 +22,11 @@ const DesignerContextProvider = ({ children }: { children: ReactNode }) => {
     });
   };
 
+  const removeElement = (id: string) => {
+    setElements((prev) => prev.filter((element) => element.id !== id));
+  };
   return (
-    <DesignerContext.Provider value={{ elements, addElement }}>
+    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
       {children}
     </DesignerContext.Provider>
   );
