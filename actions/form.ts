c4225aa7e819d31c2a42cca9549c87061bbbb496
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-06-01 12:08:49 +0200

    setting up prisma

---
+++ actions/form.ts
@@ -0,0 +1,83 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+import { formSchema, formSchemaType } from '@/schemas/form';
+import { currentUser } from '@clerk/nextjs/server';
+
+class UserNotFoundErr extends Error {}
+
+export const GetFormStats = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = stats._sum.visits || 0;
+  const submissions = stats._sum.submissions || 0;
+
+  const submissionRate = visits > 0 ? (submissions / visits) * 100 : 0;
+  const bounceRate = 100 - submissionRate;
+
+  return { visits, submissions, submissionRate, bounceRate };
+};
+
+export const CreateForm = async (data: formSchemaType) => {
+  const validation = formSchema.safeParse(data);
+  if (!validation.success) {
+    throw new Error('Form not valid');
+  }
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const { name, description } = data;
+  const form = await prisma.form.create({
+    data: { userId: user.id, name, description },
+  });
+
+  if (!form) {
+    throw new Error('Something went wrong');
+  }
+  return form.id;
+};
+
+export const GetForms = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+};
+
+export const GetFormById = async (id: number) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+  return await prisma.form.findUnique({
+    where: {
+      userId: user.id,
+      id,
+    },
+  });
+};
+
