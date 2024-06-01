2ccaaa4489ac00bfb3b9f490878e3dc277545504
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-06-01 13:05:23 +0200

    Fixed sign In Issue

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,31 @@
 import Logo from '@/components/Logo';
 import ThemeSwitcher from '@/components/ThemeSwitcher';
-import { UserButton } from '@clerk/nextjs';
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
-    <nav className='flex justify-between items-center border-b border-border h-[60px] px-4 py-2'>
-      <Logo />
-      <div className='flex gap-4 items-center'>
-        <ThemeSwitcher />
-        <UserButton afterSignOutUrl='/sign-in' />
+  <>
+    <SignedIn>
+      <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
+        <nav className='flex justify-between items-center border-b border-border h-[60px] px-4 py-2'>
+          <Logo />
+          <div className='flex gap-4 items-center'>
+            <ThemeSwitcher />
+            <UserButton afterSignOutUrl='/sign-in' />
+          </div>
+        </nav>
+        <main className='flex w-full flex-grow'>{children}</main>
       </div>
-    </nav>
-    <main className='flex w-full flex-grow'>{children}</main>
-  </div>
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </>
 );
 
 export default Layout;
