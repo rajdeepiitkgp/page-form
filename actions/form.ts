1b24c57a3df200e40b9243568314c371acf72190
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-06-02 10:48:17 +0200

    Implement Publish Button

--- actions/form.ts
+++ actions/form.ts
@@ -97,3 +97,20 @@ export const UpdateFormContent = async (id: number, jsonElements: string) => {
   });
 };
 
+export const PublishForm = async (id: number) => {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  return await prisma.form.update({
+    data: {
+      published: true,
+    },
+    where: {
+      userId: user.id,
+      id,
+    },
+  });
+};
+
